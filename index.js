b896a7be8e4de94f6a1e9db39af1552034031884
Ben Spector <beniyey@gmail.com>
2024-08-29 14:21:18 +0300

    added cors config

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-    origin: ['https://havana-backend-app-735f5839e20d.herokuapp.com'],
+    origin: ['https://havana-backend-app-735f5839e20d.herokuapp.com', "http://localhost:3000"],
     credentials: true
 }
 app.use(cors(corsOptions))
