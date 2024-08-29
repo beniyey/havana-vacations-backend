a9e9af84036951d1d0dfe44957a8f5bb38a2f12b
Ben Spector <beniyey@gmail.com>
2024-08-29 14:09:30 +0300

    added default test route

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
+app.get("/", (req, res) => {
+    res.send("hi, this is working")
+})
+
 app.use("/auth", authRouter)
 app.use("/vacations", vacationsRouter)
 
