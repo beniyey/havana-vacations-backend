99a1632267a8a413e7fc962e3a6cfd241b3da5ad
Ben Spector <beniyey@gmail.com>
2024-09-02 14:34:16 +0300

    ff

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import cors from "cors"
 import fs from "fs"
 import path from "path"
 import { dirname } from 'path';
+import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
